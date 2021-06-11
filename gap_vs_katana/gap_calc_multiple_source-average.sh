2a71b0184345f0c802f20f2551411574821dfad8
Hochan Lee <hochan@katanagraph.com>
2021-06-11 02:01:53 -0700

    Fix gap result parser

--- gap_vs_katana/gap_calc_multiple_source-average.sh
+++ gap_vs_katana/gap_calc_multiple_source-average.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-NUM_START_POINT=3
+NUM_START_POINT=64
 
 #for file in sssp*.out; do
-for file in bfs/*; do
-  ALLTIME=(`grep -Po '(?<=Trial Time:).*' $file`)
+for input in "kron" "road" "urand" "twitter" "web"; do 
   total_str="("
-  for line in ${ALLTIME[@]}; do
-    line=`echo $line | xargs`
-    IFS=" "
-    read -ra line <<< $line
-    for time in "${line[@]}"; do
-      total_str+=${time}" + "
+  for file in bfs/bfs-${input}*; do
+    ALLTIME=(`grep -Po '(?<=Trial Time:).*' $file`)
+    for line in ${ALLTIME[@]}; do
+      line=`echo $line | xargs`
+      IFS=" "
+      read -ra line <<< $line
+      for time in "${line[@]}"; do
+        total_str+=${time}" + "
+      done
     done
   done
-  total_str+=" 0)/${NUM_START_POINT}"
+  total_str+=" 0)/(3*${NUM_START_POINT})"
+  echo $total_str
   total=`echo "scale=5; ${total_str}" | bc`
-  echo $file":"$total >> time.out
+  echo $file"+"$input":"$total >> time.out
 done
